5368b8b2d35e0940a4ae680d6961801ec77b4c4f
ami-GS <1991.daiki@gmail.com>
2018-08-19 18:13:37 -0700

    write methods

--- cdds.go
+++ cdds.go
@@ -84,6 +84,16 @@ func (w Writer) Write(data unsafe.Pointer) {
 	ErrorCheck(ret, C.DDS_CHECK_REPORT|C.DDS_CHECK_EXIT, "tmp where")
 }
 
+func (w Writer) WriteTimeStampe(data unsafe.Pointer, ts Time) {
+	ret := C.dds_write_ts(C.dds_entity_t(w), data, C.dds_time_t(ts))
+	ErrorCheck(ret, C.DDS_CHECK_REPORT|C.DDS_CHECK_EXIT, "tmp where")
+}
+
+func (w Writer) WriteDispose(data unsafe.Pointer) {
+	ret := C.dds_writedispose(C.dds_entity_t(w), data)
+	ErrorCheck(ret, C.DDS_CHECK_REPORT|C.DDS_CHECK_EXIT, "tmp where")
+}
+
 func CreateReader(participant EntityI, topic Topic, qos *QoS, listener *Listener) Reader {
 	tmp := C.dds_create_reader(participant.GetEntity(), (C.dds_entity_t)(topic), (*C.dds_qos_t)(qos), (*C.dds_listener_t)(listener))
 	ErrorCheck(tmp, C.DDS_CHECK_REPORT|C.DDS_CHECK_EXIT, "tmp where")
