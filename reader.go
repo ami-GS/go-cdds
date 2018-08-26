bde78aefa4021304251e321202d8b9564d57e3fb
ami-GS <1991.daiki@gmail.com>
2018-08-26 14:07:27 -0700

    return Array

--- reader.go
+++ reader.go
@@ -21,38 +21,54 @@ func (r Reader) Read(samples *unsafe.Pointer, info *SampleInfo, bufsz int, maxsz
 	return Return(ret)
 }
 
-func (r Reader) ReadWithCallback(info *SampleInfo, bufsz int, maxsz uint32, finCh *chan struct{}, callback func(unsafe.Pointer)) {
+func (r Reader) ReadWithCallback(bufsz int, maxsz uint32, finCh *chan error, callback func(*Array)) {
 	// WARN: currently this might have issue when participant.Delete()
-	samples := r.BlockAllocRead(info, bufsz, maxsz)
+	// TODO: allock first, then use with loop
+	// TODO: need choise this to run forever
+	samples, err := r.BlockAllocRead(bufsz, maxsz)
+	if err != nil {
+		*finCh <- err
+	}
 	callback(samples)
-	r.allocator.Free(samples)
-	*finCh <- struct{}{}
+	r.allocator.Free(unsafe.Pointer(samples.arr))
+	*finCh <- nil
 
 }
 
-func (r Reader) BlockAllocRead(info *SampleInfo, bufsz int, maxsz uint32) unsafe.Pointer {
+func (r Reader) BlockAllocRead(bufsz int, maxsz uint32) (*Array, error) {
 	// this is not GCed by Golang, maybe
-	samples := r.allocator.Alloc(maxsz)
+	samples := r.allocator.AllocArray(maxsz)
 
 	var ret C.dds_return_t
-	for {
-		ret = C.dds_read(r.GetEntity(), &samples, (*C.dds_sample_info_t)(info), C.size_t(bufsz), C.uint32_t(maxsz))
-		if info.IsValid() {
+	for i := 0; i < bufsz; {
+		loc := samples.At(i)
+		info := (*C.dds_sample_info_t)(samples.InfoAt(i))
+
+		ret = C.dds_read(r.GetEntity(), &loc, info, C.size_t(bufsz), C.uint32_t(maxsz))
+		if ret < 0 {
+			return nil, CddsErrorType(ret)
+		}
+
+		if info.valid_data {
+			i++
 			break
 		}
 		time.Sleep(time.Millisecond * 20)
 	}
-	ErrorCheck(ret, C.DDS_CHECK_REPORT|C.DDS_CHECK_EXIT, "tmp where")
-	return samples
+
+	return samples, nil
 }
 
-func (r Reader) AllocRead(info *SampleInfo, bufsz int, maxsz uint32) unsafe.Pointer {
+func (r Reader) AllocRead(bufsz int, maxsz uint32) (*Array, error) {
 	// this is not GCed by Golang, maybe
-	samples := r.allocator.Alloc(maxsz)
+	samples := r.allocator.AllocArray(maxsz)
+	loc := samples.At(0)
 
-	ret := C.dds_read(r.GetEntity(), &samples, (*C.dds_sample_info_t)(info), C.size_t(bufsz), C.uint32_t(maxsz))
-	ErrorCheck(ret, C.DDS_CHECK_REPORT|C.DDS_CHECK_EXIT, "tmp where")
-	return samples
+	ret := C.dds_read(r.GetEntity(), &loc, (*C.dds_sample_info_t)(samples.InfoAt(0)), C.size_t(bufsz), C.uint32_t(maxsz))
+	if ret < 0 {
+		return nil, CddsErrorType(ret)
+	}
+	return samples, nil
 }
 
 func (r Reader) CreateReadCondition(mask uint32) ReadCondition {
