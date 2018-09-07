2e1410a1c36f41878132bf0f7f2bd4abb9e5af9a
ami-GS <1991.daiki@gmail.com>
2018-09-06 18:53:41 -0700

    fix bug of InfoAt()

--- array.go
+++ array.go
@@ -24,7 +24,7 @@ func (a RawArray) At(idx int) unsafe.Pointer {
 		panic("segmentation fault")
 	}
 
-	return unsafe.Pointer(uintptr(a.head) + uintptr(uint32(idx)*a.arraySize))
+	return unsafe.Pointer(uintptr(a.head) + uintptr(uint32(idx)*a.elmSize))
 }
 
 func (a RawArray) ForEach(fn func(unsafe.Pointer)) {
@@ -41,18 +41,21 @@ func (info *SampleInfo) IsValid() bool {
 
 type Array struct {
 	*RawArray
-	infos unsafe.Pointer
+	infoElmSize uint32
+	infos       unsafe.Pointer
 }
 
 // Should be called from allocator
 func NewArray(arrayHead unsafe.Pointer, infoHead unsafe.Pointer, bufSize uint32, elmSize uint32) *Array {
+	var info C.dds_sample_info_t
 	return &Array{
 		RawArray: &RawArray{
 			head:      arrayHead,
 			elmSize:   elmSize,
 			arraySize: bufSize,
 		},
-		infos: infoHead,
+		infoElmSize: uint32(unsafe.Sizeof(info)),
+		infos:       infoHead,
 	}
 }
 
@@ -60,8 +63,7 @@ func (a Array) InfoAt(idx int) *SampleInfo {
 	if uint32(idx) >= a.arraySize {
 		panic("segmentation fault")
 	}
-
-	return (*SampleInfo)(unsafe.Pointer(uintptr(a.infos) + uintptr(uint32(idx)*a.arraySize)))
+	return (*SampleInfo)(unsafe.Pointer(uintptr(a.infos) + uintptr(uint32(idx)*a.infoElmSize)))
 }
 
 func (a Array) IsValidAt(idx int) bool {
